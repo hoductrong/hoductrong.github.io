2b805e9f91168b1876c09297e1b60b344d607f4b
Trong Ho <trong.hoduc@gmail.com>
2023-04-26 17:39:46 +0700

    WIP: add main ui

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import "../app.css";
+</script>
+<style >
+</style>
+
+<div class="shadow-[0px 4px 4px rgba(0, 0, 0, 0.25)] bg-[#011627] m-20 rounded-lg border border-solid border-[#1E2D3D]">
+  <div class="header p-4 text-[#607B96] flex flex-row justify-between">
+    <span class="author-name">trong-ho</span>
+    <div class="">
+      <a class="py-4 pr-8 pl-8 border-solid border border-[#1E2D3D]" href="#">_hello</a>
+      <a href="#">_about-me</a>
+      <a href="#">_projects</a>
+    </div>
+    <a href="#">_contact-me</a>
+  </div>
+  <div class="main-container">
+  </div>
+</div>
