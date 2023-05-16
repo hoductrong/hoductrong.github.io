aa7daf3e300fe53e9b47820361d631cfd0bc7a7b
Trong Ho <trong.hoduc@gmail.com>
2023-05-16 19:30:40 +0700

    add sidebar

---
+++ src/components/SideBar.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import type { SideBarItem, SideBarItemClickProps } from '../types';
+	import SideBarTreeItem from './SideBarTreeItem.svelte';
+	import {
+		TerminalBoxFillDEVELOPMENT,
+		User4FillUSER,
+		GamepadFillDEVICE,
+		Folder3FillDOCUMENT,
+		MarkdownFillDOCUMENT
+	} from 'svelte-remix';
+
+	const onItemClick = (item: SideBarItemClickProps) => {
+		console.log(item);
+	};
+
+	const items: SideBarItem[] = [
+		{
+			name: 'personal-info',
+			onItemClick,
+			childs: [
+				{
+					name: 'bio',
+					icon: Folder3FillDOCUMENT,
+					onItemClick
+				},
+				{
+					name: 'interests',
+					icon: Folder3FillDOCUMENT,
+					onItemClick
+				},
+				{
+					name: 'education',
+					icon: Folder3FillDOCUMENT,
+					onItemClick,
+					childs: [
+						{
+							name: 'high-school',
+							icon: MarkdownFillDOCUMENT,
+							onItemClick
+						},
+						{
+							name: 'university',
+							icon: MarkdownFillDOCUMENT,
+							onItemClick
+						}
+					]
+				}
+			]
+		},
+		{
+			name: 'contacts',
+			onItemClick,
+			childs: [
+				{
+					name: 'user@gmail.com',
+					icon: MarkdownFillDOCUMENT,
+					onItemClick
+				},
+				{
+					name: '+396790841',
+					icon: MarkdownFillDOCUMENT,
+					onItemClick
+				}
+			]
+		}
+	];
+</script>
+
+<div class="left-sidebar border border-solid border-[#1E2D3D] w-80 flex flex-row text-[#607B96]">
+	<div class="tabs-list flex flex-col pl-6 pr-5 border border-solid border-[#1E2D3D]">
+		<div class="py-4">
+			<TerminalBoxFillDEVELOPMENT size="24px" />
+		</div>
+		<div class="py-4">
+			<User4FillUSER size="24px" />
+		</div>
+		<div class="py-4">
+			<GamepadFillDEVICE size="24px" />
+		</div>
+	</div>
+	<SideBarTreeItem {items} />
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,25 +1,13 @@
 <script lang="ts">
-  import Layout from '../__layout.svelte';
-  import {TerminalBoxFillDEVELOPMENT, User4FillUSER, GamepadFillDEVICE} from 'svelte-remix'
+	import Layout from '../__layout.svelte';
+	import SideBar from '../../components/SideBar.svelte';
 </script>
-<style >
-</style>
 
 <Layout>
-  <div class="">
-    <div class="left-sidebar flex flex-row text-[#607B96]">
-      <div class="tabs-list flex flex-col pl-6 pr-5 border border-solid border-[#1E2D3D]">
-        <div class="py-4">
-          <TerminalBoxFillDEVELOPMENT size='24px'/>
-        </div>
-        <div class="py-4">
-          <User4FillUSER size='24px'/>
-        </div>
-        <div class="py-4">
-          <GamepadFillDEVICE size='24px'/>
-        </div>
-      </div>
+	<div class="">
+		<SideBar />
+	</div>
+</Layout>
 
-    </div>
-  </div>
-</Layout>5
+<style>
+</style>
